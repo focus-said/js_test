4365b9a94e37db7585c7ff24bc5f616db73bacf7
focus_said <1035667191@qq.com>
2022-06-28 14:31:38 +0800

    add 03

--- 03test.py
+++ 03test.py
@@ -6,3 +6,28 @@
 """
 
 
+class Demo:
+
+    class_name = ''
+
+
+a1 = Demo()
+a2 = Demo()
+a1.child = a2
+a2.child = a1
+
+"""
+循环引用 导致到内存泄露 主要是引用计数无法清0
+"""
+from selenium import webdriver
+
+
+webdriver_client = webdriver.Chrome()
+webdriver_client.get("www.baidu.com")
+webdriver_client.close()
+# webdriver_client.quit()
+
+
+"""
+没有正常关闭导致的Chrome webdriver进程依然在
+"""
